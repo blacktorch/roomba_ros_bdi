cb77b4f867bb44f413cdef68afdea014e402ed54
blacktorch <codebreakersmd@gmail.com>
2020-01-09 23:06:08 -0500

    initial commit

---
+++ src/main/java/com/github/roomba_bdi/roomba_ros_bdi/SaviAgentArch.java
@@ -0,0 +1,63 @@
+//package com.github.savi_ros_java.savi_ros_bdi;
+
+package com.github.rosjava.roomba_bdi.roomba_ros_bdi;
+
+import org.apache.commons.logging.Log;
+import org.ros.message.MessageListener;
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
+import org.ros.node.NodeMain;
+import org.ros.node.topic.Subscriber;
+
+/**
+ * Based upon a class found in SAVI: https://github.com/NMAI-lab/SAVI
+ *
+ * @author Patrick Gavigan
+ * @date 6 December 2019
+ */
+public class SaviAgentArch {
+
+    private SaviAgent theAgent;
+    private ActionTalker actionTalker;
+
+    /**
+     * Need a connectedNode, can't have default constructor
+     */
+    private SaviAgentArch(){}
+
+    /**
+     * Creates the Jason MAS Builder
+     * For now there's only one type of Jason agent in the sense of its capabilities towards the environment
+     * (go, stop, turn right, turn left) but each agent can have its plans. The plans should be in a file
+     * type.asl where type is the agent attribute "type".
+     */
+    public SaviAgentArch(final ConnectedNode connectedNode) {
+        // Build the agent
+        this.theAgent = new SaviAgent("0", "demo");
+
+        // Setup the action talker, for replying with actions at the end of the reasoning cycle
+        this.actionTalker = new ActionTalker(connectedNode);
+    }
+
+    /**
+     * Start the agent.
+     */
+    public void startAgents() {
+
+        // Run the agent thread
+        Thread agentThread = new Thread(this.theAgent);
+        agentThread.start();
+
+        // Run the action talking thread
+        Thread actionTalingThread = new Thread(this.actionTalker);
+        actionTalingThread.start();
+    }
+
+    /**
+     * TODO: check what this actually does...
+     */
+    void stopAgents() {
+        theAgent.stop();
+    }
+}
---
+++ src/main/java/com/github/roomba_bdi/roomba_ros_bdi/SyncAgentState.java
@@ -0,0 +1,101 @@
+package com.github.rosjava.roomba_bdi.roomba_ros_bdi;
+
+/**
+ * Singleton class for passing perceptions, actions, messages between the agent and the environment.
+ *
+ * Based upon a class found in SAVI: https://github.com/NMAI-lab/SAVI
+ *
+ * @author Patrick Gavigan
+ * @date 6 December 2019
+ */
+public class SyncAgentState {
+
+    // Location for the perception to be stored. Replace with a queue later
+    private String perception;
+    private boolean perceptionAvailable;
+
+    // Location for the action to be stored. Replace with a queue later
+    private String action;
+    private boolean actionAvailable;
+
+    // Static instance of this singleton class
+    private static SyncAgentState agentState;
+
+    /**
+     * Mehthod for accessing this singleton class
+     * @return      Reference to the singleton object
+     */
+    public static SyncAgentState getSyncAgentState() {
+        if (SyncAgentState.agentState == null) {
+            SyncAgentState.agentState = new SyncAgentState();
+
+        }
+        return SyncAgentState.agentState;
+    }
+
+
+    /**
+     *  Constructor for the SyncAgentState class.
+     *  This is a private constructor as this is a singleton class.
+     */
+    private SyncAgentState() {
+        this.perceptionAvailable = false;
+        this.actionAvailable = false;
+    }
+
+
+    /**
+     * Check if there is a fresh perception.
+     * @return
+     */
+    public synchronized boolean isPerceptionAvailable() {
+        return this.perceptionAvailable;
+    }
+
+    /**
+     * Set the perceptions, as received from the sensors (environment side)
+     * @param newPerception
+     */
+    public synchronized void setPerceptions(String newPerception) {
+        this.perception = String.valueOf(newPerception);
+        this.perceptionAvailable = true;
+    }
+
+    /**
+     * Get the perceptions. (Agent side)
+     * @return
+     */
+    public synchronized String getPerceptions() {
+        if (isPerceptionAvailable()) {
+            this.perceptionAvailable = false;
+            return this.perception;
+        } else {
+            return null;
+        }
+    }
+
+    public synchronized boolean isActionAvailable() {
+        return this.actionAvailable;
+    }
+
+    /**
+     * Provide the action the agent wants to execute (Agent side)
+     * @param newAction
+     */
+    public synchronized void addAction(String newAction) {
+        this.action = String.valueOf(newAction);
+        this.actionAvailable = true;
+    }
+
+    /**
+     * Get the action that the agent has requested
+     */
+    public synchronized String getAction() {
+        if (isActionAvailable()) {
+            this.actionAvailable = false;
+            return this.action;
+        } else {
+            return null;
+        }
+    }
+}
